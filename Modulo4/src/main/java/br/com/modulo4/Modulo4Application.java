fe05f968606fb947512b62737b208aaf65fd3abf
yellow-Umbrella <renanbenattidias@gmail.com>
2016-08-15 06:03:50 -0400

    Persist and xhtml test not working (Target Unreachable, identifier 'clientController' resolved to null)

--- Modulo4/src/main/java/br/com/modulo4/Modulo4Application.java
+++ Modulo4/src/main/java/br/com/modulo4/Modulo4Application.java
@@ -2,9 +2,16 @@ package br.com.modulo4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Modulo4Application {
+public class Modulo4Application extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Modulo4Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Modulo4Application.class, args);
---
+++ Modulo4/src/main/java/controller/ClientController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import javax.faces.bean.ViewScoped;
+import javax.inject.Named;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.modulo4.models.Client;
+import br.com.modulo4.repository.ClientRepository;
+
+@Named
+@ViewScoped
+public class ClientController {
+	
+	@Autowired
+	private ClientRepository cliRepository;
+	
+	private Client client = new Client();
+	
+	public void save() {
+		cliRepository.save(getClient());
+	}
+	
+	public Client getClient() {
+		return client;
+	}
+	
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+}
